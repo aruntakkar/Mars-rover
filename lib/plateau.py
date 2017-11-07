ba5802b0fc1ecab53a7061696c668bba7c49936f
aruntakkar5@gmail.com <9693884+aruntakkar@users.noreply.github.com>
2017-11-08 01:09:49 +0530

    "Added Plateau with tests"

---
+++ lib/plateau.py
@@ -0,0 +1,23 @@
+# module for plateau
+
+"""
+ Plateau is an object that has bounding coordinates 
+ for bottom left and top right with which it is 
+ initialized. Additionally it can help check whether
+ a given pair of coordinates is within Pleatue or not.
+"""
+
+
+class plateau:
+    def __init__(self, bounding_coordinates):
+        self.bound = [bounding_coordinates[0], bounding_coordinates[1],
+                      bounding_coordinates[2], bounding_coordinates[3]]
+
+    def is_inside_plateau(self, coordinates):
+        x, y = int(coordinates[0]), int(coordinates[1])
+
+        if ((x >= self.bound[0] and y >= self.bound[1]) and
+                (x <= self.bound[2] and y <= self.bound[2])):
+            return True
+        else:
+            return False
---
+++ tests/test_plateau.py
@@ -0,0 +1,23 @@
+import unittest
+from lib.plateau import plateau
+
+
+class PlateauTestCase(unittest.TestCase):
+
+    def test_is_inside_plateau(self):
+        coordinates = [2, 2]
+        bounds = [0, 0, 5, 5]
+        plateau_object = plateau(bounds)
+        self.assertTrue(plateau_object.is_inside_plateau(coordinates))
+
+    def test_with_valid_input(self):
+        coordinates = [5, 5]
+        bounds = [0, 0, 5, 5]
+        plateau_object = plateau(bounds)
+        self.assertTrue(plateau_object.is_inside_plateau(coordinates))
+
+    def test_with_invalid_input(self):
+        coordinates = [-5, 5]
+        bounds = [0, 0, 5, 5]
+        plateau_object = plateau(bounds)
+        self.assertFalse(plateau_object.is_inside_plateau(coordinates))
